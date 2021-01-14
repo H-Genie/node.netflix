5a4355206639eda796be6f864d98ac6ad4517ae5
H-Genie <61546294+H-Genie@users.noreply.github.com>
2021-01-14 11:34:50 +0900

    Add files via upload

--- database.js
+++ database.js
@@ -1,13 +1,13 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 // SQL 연결
 module.exports = mysql.createPool({
-    connectionLimit : 10,
-    host : 'nodejs-007.cafe24.com',
-    user : 'h0genie',
-    password : 'st227213314!@',
-    database : 'h0genie',
-    debug : false
+    connectionLimit: 10,
+    host: "us-cdbr-east-02.cleardb.com",
+    user: "b0bfe1f0e5ca95",
+    password: "18256dda",
+    database: "heroku_35ac0793c3cf4c1",
+    debug: false,
 });
 
 /*
