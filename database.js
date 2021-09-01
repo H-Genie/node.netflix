fb9a741c31b565689aef1184d1364e2b2978186f
H-Genie <61546294+H-Genie@users.noreply.github.com>
2021-09-01 19:07:52 +0900

    Update database.js

--- database.js
+++ database.js
@@ -1,21 +1,11 @@
 const mysql = require("mysql");
 
-// SQL 연결
-// module.exports = mysql.createPool({
-//     connectionLimit : 10,
-//     host : 'nodejs-007.cafe24.com',
-//     user : 'h0genie',
-//     password : 'st227213314!@',
-//     database : 'h0genie',
-//     debug : false
-// });
-
 module.exports = mysql.createPool({
     connectionLimit: 10,
-    host: "us-cdbr-east-02.cleardb.com",
-    user: "b0bfe1f0e5ca95",
-    password: "18256dda",
-    database: "heroku_35ac0793c3cf4c1",
+    host: "us-cdbr-east-04.cleardb.com",
+    user: "bace9f6ecebfee",
+    password: "f862a6c1",
+    database: "heroku_6a433ca724d630e",
     debug: false,
 });
 
